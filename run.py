3e634f0f5b275f9bc99f7dc2d8eeeed6c2461247
Renzo <api@gmail.com>
2025-11-06 13:19:02 -0300

    Configuración de puerto para Render

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app.routes import routes
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
     print("App iniciando...")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
